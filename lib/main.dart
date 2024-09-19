19c6be4c0588be41084775ad687801830fe5f780
Normando Meira - Lab. Fatec <normando.meira@fatec.sp.gov.br>
2024-09-19 20:02:09 -0300

    Teste de commit

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+import 'package:device_preview/device_preview.dart';
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
